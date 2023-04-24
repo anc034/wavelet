4cc18fcb11804e8698384ed63d2e9fb4d4103e5e
anc034 <chavez21andre@gmail.com>
2023-04-23 21:56:09 -0700

    New file that contains the code from Lab Report #2 String Server

--- SearchEngine.java
+++ SearchEngine.java
@@ -3,24 +3,39 @@ import java.net.URI;
 import java.util.ArrayList;
 
 class SearchEngine implements URLHandler{
-    ArrayList<String> str = new ArrayList<>();
+    ArrayList<String> result = new ArrayList<>();
 
     public String handleRequest(URI url) {
-        String result = "";
+        String str = "";
         if(url.getPath().equals("/")){
-           for(int i = 0; i < str.size(); i++){
-           if(i > 0){
-            result += " and ";
-           }
-           result = result + str.get(i);
+           for(int i = 0; i < result.size(); i++){
+            str = str + result.get(i) + "\n";
+            System.out.println(str);
         }
+        return(str);
     }
 
-        else if(url.getPath().equals("/add")){
+        else if(url.getPath().equals("/add-message")){
             String[] parameters = url.getQuery().split("=");
             if(parameters[0].equals("s")){
-                str.add(parameter[1]);
+                result.add(parameters[1]);
+                return("String " + parameters[1] + " added to server.");
             }
         }
+        return("404 not found");
+    }
+}
+
+class StringServer{ 
+    public static void main(String[] args) throws IOException{
+        if(args.length == 0){
+            System.out.println("Missing port Number! Please try any number between 1024 and 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new SearchEngine());
+
     }
 }
